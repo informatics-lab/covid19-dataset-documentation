5c67923fa9af4c0d58edcba7894ff2cc66899ba9
Theo McCaie <tam203@gmail.com>
2020-04-18 20:57:18 +0100

    pass throught deploy args

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_DIR=$SCRIPT_DIR/..
 
-python $SCRIPT_DIR/build_readme_and_index_html.py
+python $SCRIPT_DIR/build_readme_and_index_html.py "$@"
