7523087fbb3914e9eb83c5f5c0c28bcef0e4df29
Barnaby Sherratt <barnaby.sherratt@metoffice.gov.uk>
2020-05-26 10:49:55 +0100

    Update deploy script for air quality
    Rearranged how BlobEntry objects are created - now added to a list as 
    soon as available, instead of maintaining a lot of locals for filenames 
    and content. Hence it's easy to loop over any number of markdown files 
    and convert them all, in particular the air quality documentation as 
    required.
    
    Also a small bugfix: HTML pages now specify UTF-8 encoding, because 
    there are UTF-8 characters in README_data but browsers were defaulting 
    to plain ASCII.

--- scripts/build_readme_and_index_html.py
+++ scripts/build_readme_and_index_html.py
@@ -15,6 +15,23 @@ DRY_RUN = False if (len(sys.argv) >= 2 and sys.argv[1].strip() == '--deploy') el
 print("### DRY RUN ###" if DRY_RUN else "### DEPLOY ###")
 
 
+# HTML Template
+HEAD = f"""<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Met Office Informatics Lab COVID-19 environmental data</title>
+</head>
+<body>
+"""
+FOOT = '</body></html>'
+
+
+# Define a list of blobs to upload
+BlobEntry = namedtuple("Blob", ['path', 'content', 'mime'])
+blobs = []
+
+
 # set up blob access
 
 SIGNED_URL = os.environ['SIGNED_URL']
@@ -33,13 +50,13 @@ container_client = blob_service_client.get_container_client(CONTAINER)
 print("\nListing blobs...")
 # List the blobs in the container
 blob_list = container_client.list_blobs()
-blobs = [blob.name for blob in blob_list]  # if blob.name[0] != '.' and blob.name.find('/.') == -1]
-print(f"found {len(blobs)} blobs")
+all_blobs = [blob.name for blob in blob_list]  # if blob.name[0] != '.' and blob.name.find('/.') == -1]
+print(f"found {len(all_blobs)} blobs")
 
 
 # filter out 'hidden' files
 clean_blobs = []
-for blob in blobs:
+for blob in all_blobs:
     if blob[0] == '.' or blob.find('/.') >= 0:
         continue
     clean_blobs.append(blob)
@@ -50,15 +67,9 @@ print(f"got {len(clean_blobs)} non hidden blobs")
 
 base = "<NOT A BASE>"
 html = """
-<html>
-    <head>
-        <title>Met Office Informatics Lab COVID-19 environmental data</title>
-        
-    </head>
-    <body>
     <h1>Met Office Informatics Lab COVID-19 environmental data index</h1>
     <p>This is a very simple index of the files in this container/bucket.<p>
-    <p>For information on this dataset see 
+    <p>For information on this dataset see
     <a href="https://medium.com/informatics-lab/met-office-and-partners-offer-data-and-compute-platform-for-covid-19-researchers-83848ac55f5f">the related blog post</a>
     and the <a href="README.md">README.md</a>.
     </p>
@@ -92,56 +103,40 @@ for i, blob in enumerate(sorted(clean_blobs, key=sorter)):
     html += htmlstr
 html += f"""</ul>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-<p style="font-size:50%">Created at {datetime.datetime.now()}</p>
-</body></html>"""
+<p style="font-size:50%">Created at {datetime.datetime.now()}</p>"""
 
-index_html = html
-
-# README.html
-README_MD_PATH_LOCAL = "./README_data.md"
+blobs.append(BlobEntry('index.html', HEAD+html+FOOT, 'text/html'))
 
 
-with open(README_MD_PATH_LOCAL) as fp:
-    markdown_readme = fp.read()
+# Convert markdown files to HTML
+for path_local in ('./README_data.md', './README_data_air_quality.md'):
+    name = os.path.splitext(os.path.basename(path_local))[0]
 
-# markdown_readme = blob_service_client.get_blob_client(container=COUNTAINER, blob=README_MD_PATH).download_blob().readall().decode('utf-8')
+    with open(path_local) as fp:
+        markdown_readme = fp.read()
 
-print(f'Markdown from {README_MD_PATH_LOCAL}:\n')
-print(markdown_readme)
-print('\n'*3)
+    # markdown_readme = blob_service_client.get_blob_client(container=COUNTAINER, blob=README_MD_PATH).download_blob().readall().decode('utf-8')
 
-head = """
-<html>
-<head>
-    <title>Met Office Informatics Lab COVID-19 environmental data</title>
-</head>
-<body>
-"""
-body = markdown(markdown_readme)
-foot = "</body></html>"
-html = (head+body+foot)
+    print(f'Markdown from {path_local}:\n')
+    print(markdown_readme)
+    print('\n'*3)
 
-readme_html = html
+    # Add raw markdown
+    blobs.append(BlobEntry(f'{name}.md', markdown_readme, 'text/markdown'))
+
+    # Add HTML
+    html = markdown(markdown_readme)
+    blobs.append(BlobEntry(f'{name}.html', HEAD+html+FOOT, 'text/html'))
 
 
-# Upload to the blob store
-INDEX_PATH = 'index.html'
-README_MD_PATH = "README_data.md"
-README_HTML_PATH = README_MD_PATH.rsplit('.')[0] + '.html'
-
-BlobEntry = namedtuple("Blob", ['path', 'content', 'mime'])
-
+# Add technical docs
 TECHNICAL_DOCS_PATH = 'README_data_processing.pdf'
 with open(TECHNICAL_DOCS_PATH, 'rb') as fp:
     technical_docs_data = fp.read()
+blobs.append(BlobEntry(TECHNICAL_DOCS_PATH, technical_docs_data, 'application/pdf'))
 
-blobs = [
-    BlobEntry(README_HTML_PATH, readme_html, 'text/html'),
-    BlobEntry(INDEX_PATH, index_html, 'text/html'),
-    BlobEntry(README_MD_PATH, markdown_readme, 'text/markdown'),
-    BlobEntry(TECHNICAL_DOCS_PATH, technical_docs_data, 'application/pdf')
-]
 
+# Upload to the blob store
 if(DRY_RUN):
     print("*** DRY RUN ***")
     for blob_entry in blobs:
